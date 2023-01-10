4575766641ef4349bc772632a4de5e1e9b1cb5ab
macbook <seaman@macbook>
2023-01-09 17:36:52 -0700

    Build launch config

--- 06/Wildlife/config/urls.py
+++ 06/Wildlife/config/urls.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
+from animals.views import AnimalsView
 from django.urls import path
 
 
-def index(request):
-    return HttpResponse("<h1>Wildlife</h1><p>I love animals</p>")
-
-
 urlpatterns = [
-    path('', index),
+    path('', AnimalsView.as_view()),
 ]
